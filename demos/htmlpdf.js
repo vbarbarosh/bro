d9520e671d74d6c2fd11df98a15c6fe649e25963
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2017-10-14 14:36:17 +0300

    add demos/htmlpdf

---
+++ demos/htmlpdf.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env bro
+
+main();
+
+async function main()
+{
+    // https://stackoverflow.com/a/7398477/1478566
+    const body = document.body;
+    const html = document.documentElement;
+    const width = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
+    // +1 is necessary to avoid last empty page
+    const height = 1 + Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    await bro_pdf(location.href.replace(/\.html$/, '') + '.pdf', {width, height, printBackground: true});
+}
